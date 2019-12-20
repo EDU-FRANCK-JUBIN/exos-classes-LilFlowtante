6a15b9450ef7e51b9bbeded2b538bdedf9c85490
LilFlowtante <44292666+LilFlowtante@users.noreply.github.com>
2019-12-20 14:23:42 +0100

    Dictionnaire

---
+++ DictionnaireConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionnaireConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dictionnaires :");
+            Console.WriteLine();
+            Dictionary<string, int> dico = new Dictionary<string, int>();
+            dico.Add("Louis", 21);
+            dico.Add("Maxime", 22);
+            dico.Add("Vincent", 21);
+            dico.Add("Aurélien", 21);
+            dico.Add("Hugo", 21);
+            dico.Add("Marjorie", 19);
+        }
+    }
+}
